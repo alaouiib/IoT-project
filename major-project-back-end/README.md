168dfbe32395aa63d14ffe3558a212ef368ca2fe
alaouiib <alaouiib.fstt@gmail.com>
2019-12-29 01:04:55 +0100

    beta

--- major-project-back-end/README.md
+++
@@ -1,2 +0,0 @@
-# major-project-back-end
-back-end developement for the major project using spring boot, JPA Hibernate
--- major-project-back-end/src/main/java/com/emse/rommsproject/repositories/RoomRepository.java
+++
@@ -1,9 +0,0 @@
-package com.emse.rommsproject.repositories;
-
-import com.emse.rommsproject.models.Room;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.rest.webmvc.RepositoryRestController;
-
-@RepositoryRestController
-public interface RoomRepository extends JpaRepository<Room, String> {
-}
